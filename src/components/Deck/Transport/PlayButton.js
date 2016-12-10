2fc63e944a6c9802668f86b753724edcbb0de185
Carl Lang <carl.m.lang@gmail.com>
2016-12-10 14:53:38 +0000

    Started refactoring of load track, need to refactor filters and source connections

--- src/components/Deck/Transport/PlayButton.js
+++ src/components/Deck/Transport/PlayButton.js
@@ -4,18 +4,66 @@ class PlayButton extends React.Component {
 
 	constructor (props) {
 		super (props);
-		this.togglePlayPause = this.togglePlayPause.bind(this);
+		this.togglePlaybackSpinUpDown = this.togglePlaybackSpinUpDown.bind(this);
 		this.state = {
 			isPressed: false
 		}
 	}
 
-	togglePlayPause () {
+	togglePlaybackSpinUpDown () {
 		let audioElement = document.querySelector('#' + this.props.deckName);
 		(audioElement.paused)? audioElement.play() : audioElement.pause();
 		this.setState({
 			isPressed: audioElement.paused
 		});
+	}
+
+	togglePlaybackSpinUpDown () {
+	    var now = audioCtx.currentTime;
+
+	//	this.cuePointIsActive = false;
+
+	    if (this.isPlaying) {
+	        //stop playing and return
+	        if (this.sourceNode) {  // we may not have a sourceNode, if our PBR is zero.
+		        var playback = this.sourceNode.playbackRate;
+		        playback.cancelScheduledValues( now );
+		        playback.setValueAtTime( playback.value, now );
+		        playback.linearRampToValueAtTime( 0.001, now+1 );
+		        this.gainNode.gain.setTargetAtTime( 0, now+1, 0.01 );
+		        this.stopTime = now;
+	 		   	this.sourceNode.stop( now + 2 );
+		        this.sourceNode = null;
+		        this.gainNode = null;
+	        }
+	        this.isPlaying = false;
+	        return false;
+	    }
+
+	    sourceNode = audioCtx.createBufferSource();
+	    sourceNode.buffer = this.buffer;
+	    sourceNode.loop = false;
+	    // The "now" below causes issues in FFnightly
+	    sourceNode.playbackRate.setValueAtTime( 0.001, now );
+	    sourceNode.playbackRate.linearRampToValueAtTime( this.currentPlaybackRate, now+1 );
+
+		this.gainNode = audioCtx.createGain();
+		this.gainNode.connect( this.filter );
+		this.gainNode.gain.value = this.gain;
+	    sourceNode.connect( this.gainNode );
+
+	    this.sourceNode = sourceNode;
+	    this.isPlaying = true;
+	    this.lastTimeStamp = now + 0.5;		// the 0.5 is to make up for the initial 1s "spin-up" ramp.
+	    this.offset = this.lastBufferTime;
+	    this.restartTime = now;
+	    this.stopTime = 0.0;
+	    this.lastPBR = this.currentPlaybackRate;
+
+	    sourceNode.onended = shutDownNodeWhenDonePlaying.bind(this);
+	    sourceNode.start( now, this.lastBufferTime );
+
+	    return true;
 	}
 
 	render () {
--- src/components/Deck/Turntable.js
+++ src/components/Deck/Turntable.js
@@ -11,6 +11,10 @@ class Turntable extends React.Component {
 		};
 		this.handleDrop = this.handleDrop.bind(this);
 		this.handleDragOver = this.handleDragOver.bind(this);
+		this.deck = {
+			name: this.props.deckName,
+			buffer: null
+		};
 	}
 
 	drawDisk () {
@@ -37,100 +41,6 @@ class Turntable extends React.Component {
 		}
 	}
 
-	// updateTurntable = function( drawOnScreen ) {
-	//     var now = audioCtx.currentTime;
-	//     var bufferTime;
-	//     var keepAnimating = this.isPlaying;
-	//
-	// 	if (!this.isPlaying) {
-	// 		if (this.stopTime) {	// still in spin-down;
-	// 			if (now > (this.stopTime + 1) ) {	// done spinning down.
-	// 				this.lastBufferTime = this.lastBufferTime + 0.5;
-	// 				this.stopTime = 0;
-	// 				return false;
-	// 			} else {
-	// 				// bufferTime = 1/2 acceleration * t^2;  // keeping acceleration = 1 simplifies this!!
-	// 				bufferTime = 1 - (now-this.stopTime);
-	// 				bufferTime = bufferTime * bufferTime;
-	// 				bufferTime = bufferTime / 2;
-	// 				bufferTime = 0.5 - bufferTime + this.lastBufferTime;
-	// 				keepAnimating = true;
-	// //				console.log( "now:" + now + " stopTime:" + this.stopTime + " bufferTime:" + bufferTime + " this.lastBufferTime:" + this.lastBufferTime );
-	// 			}
-	// 		} else
-	// 			bufferTime = this.lastBufferTime;
-	// 	} else if ((this.restartTime + 1) > now) {	// we're still in "spin-up"
-	// 		// bufferTime = 1/2 acceleration * t^2;  // acceleration = 1
-	// 		bufferTime = now-this.restartTime;
-	// 		bufferTime = bufferTime * bufferTime;
-	// 		bufferTime = bufferTime / 2;
-	// 		bufferTime += this.offset;
-	//     } else {
-	// 		this.updateTime( now );
-	// 		bufferTime = this.lastBufferTime;
-	// 	}
-	//
-	// 	if (drawOnScreen) {
-	// 		var radians = ((bufferTime * REVPERSEC) % 1) * 2 * Math.PI;
-	// 		var context = this.platterContext;
-	//
-	// 		context.clearRect(-150,-150,300,300);  // TODO: shouldn't hardcode
-	//
-	//       	context.rotate( radians );
-	// 		context.fillStyle = "white";
-	// 		context.fillText("wubwubwub",-61,8);
-	//       	context.rotate( -radians );
-	//
-	// 		if (this.buffer) {
-	// 			// Now draw the position in the buffer
-	//
-	// 			var w = this.trackDisplayCanvas.width;
-	// 			var h = this.trackDisplayCanvas.height;
-	// 			var ctx = this.trackDisplayCanvas.getContext('2d');
-	// 			ctx.clearRect(0,0,w,h);
-	// 		    ctx.drawImage( this.bufferCanvas, 0, 0 );
-	// 			var boxWidth = w * bufferTime / this.buffer.duration;
-	// 			ctx.fillStyle = "rgba(255,255,255,0.33)";
-	// 			ctx.fillRect(0,0,boxWidth,h);
-	//
-	// 			for (var i=0; i<4; i++) {
-	// 				var cue = this.cues[i];
-	// 				if (cue ) {
-	// 					var x = cue.time / this.buffer.duration * w;
-	// 					ctx.fillStyle = cueColors[i];
-	// 					ctx.fillRect( x, 0, 1, h );
-	// 					ctx.font = "12px bold Skia, Arial, sans-serif";
-	// 					ctx.fillText( cueText[i], x+2, h/4 );
-	// 				}
-	// 			}
-	//
-	// 			drawRunningDisplay( runningDisplayContext, this.waveformDisplayCache, bufferTime );
-	//
-	// 		    // draw the center bar
-	// 		    var isTop = this.isLeftTrack;
-	// 		    ctx = runningDisplayContext;
-	// 		    runningDisplayContext.fillStyle = "gray";
-	// 		    runningDisplayContext.fillRect(RUNNING_DISPLAY_HALF_WIDTH,isTop?0:RUNNING_DISPLAY_HALF_HEIGHT,1,RUNNING_DISPLAY_HALF_HEIGHT);
-	//
-	// 			// draw cues on the running display
-	// 			var begin = bufferTime - (SECONDS_OF_RUNNING_DISPLAY/2);
-	// 			var end = begin + SECONDS_OF_RUNNING_DISPLAY;
-	// 			for (var i=0; i<4; i++) {
-	// 				var cue = this.cues[i];
-	// 				if (cue && (cue.time>begin) && (cue.time<end)) {
-	// 					var x = (cue.time-begin) * RUNNING_DISPLAY_WIDTH / SECONDS_OF_RUNNING_DISPLAY;
-	// 					ctx.fillStyle = cueColors[i];
-	// 					ctx.fillRect( x, isTop ? 0 : RUNNING_DISPLAY_HALF_HEIGHT, 1, RUNNING_DISPLAY_HALF_HEIGHT );
-	// 					ctx.font = "12px bold Skia, Arial, sans-serif";
-	// 					ctx.fillText( cueText[i], x+2, isTop ? RUNNING_DISPLAY_HALF_HEIGHT/2 : RUNNING_DISPLAY_HALF_HEIGHT*1.5 );
-	// 				}
-	// 			}
-	//
-	// 		}
-	// 	}
-	//
-	// 	return keepAnimating;	// "keep animating" - may need to check if !isplaying
-	// }
 
 	handleDragOver (e) {
 		e.preventDefault();
@@ -144,7 +54,7 @@ class Turntable extends React.Component {
 		let files = e.dataTransfer.files;
 		let _this = this;
 
-		this.props.webaudio.loadTrack(files, this.props.deckName, function(result) {
+		this.loadTrack(files, this.props.deckName, function(result) {
 			if (result){
 				_this.setState({
 					trackName: result.name + ' ' + (result.size/1000000).toFixed(2) + 'mb'
@@ -155,6 +65,47 @@ class Turntable extends React.Component {
 				});
 			}
 		});
+
+	}
+
+	loadTrack (files, deckName, callback) {
+		let _this = this;
+		for (let i = 0; i < files.length; i++) {
+			let file = files[i];
+			let reader = new FileReader();
+			// reader.readAsArrayBuffer(file);
+			// reader.addEventListener('loadend', function(buffer){
+			//
+			// 	document.querySelector('#' + deckName).src = null;
+			//
+			// 	let audio = document.querySelector('#' + deckName);
+			// 	let objUrl = URL.createObjectURL(file);
+			// 	audio.src = objUrl;
+			// 	let source = _this.audioContext.createMediaElementSource(audio);
+			// 	source.connect(_this.filters[deckName]['hp'].filter);
+			// 	_this.filters[deckName]['hp'].filter.connect(_this.filters[deckName]['bp'].filter);
+			// 	_this.filters[deckName]['bp'].filter.connect(_this.filters[deckName]['lp'].filter);
+			// 	_this.filters[deckName]['lp'].filter.connect(_this.gainNode[deckName]);
+			// 	_this.gainNode[deckName].connect(_this.crossFadeGainNode[deckName]);
+			// 	_this.crossFadeGainNode[deckName].connect(_this.masterGain);
+			// 	_this.masterGain.connect(_this.analyser);
+			// 	_this.analyser.connect(_this.audioContext.destination);
+			// 	callback(file);
+			//
+			// });
+
+			reader.onload = function (event) {
+		  		_this.props.webaudio.audioContext.decodeAudioData( event.target.result, function(buffer) {
+					// if (thisTrack.isPlaying)
+					// 	thisTrack.togglePlayback();
+					_this.deck.buffer = buffer;
+					callback(file);
+					//thisTrack.postLoadTasks();
+		  		}, function(){alert("error loading!");} );
+
+		  	};
+			reader.readAsArrayBuffer(file);
+		}
 	}
 
 	componentDidMount () {
@@ -175,7 +126,6 @@ class Turntable extends React.Component {
 	}
 
 	render () {
-		//console.log()
 		return (
 			<div>
 				<h4>{this.state.trackName}</h4>
