0783d772e324859080b93e3c0f8feb8703f5255f
Carl Lang <carl.m.lang@gmail.com>
2017-12-20 15:33:18 +0000

    cleaning up

--- src/routes/Stage/components/StageView.js
+++ src/routes/Stage/components/StageView.js
@@ -1,36 +1,33 @@
 import React from 'react'
 import './StageView.scss'
 import DeckContainer from '../../../containers/DeckContainer'
-import Mixer from '../../../components/Mixer';
-import WebAudio from '../../../webAudio';
-import { AudioWave } from '../../../components/Visualizer';
+import Mixer from '../../../components/Mixer'
+import WebAudio from '../../../webAudio'
 
-const webAudio = new WebAudio();
-const decks = [{
-					name: 'deckA'
-				},
-				{
-					name: 'deckB'
-				}];
+const webAudio = new WebAudio()
+const decks = [
+  {
+    name: 'deckA'
+  },
+  {
+    name: 'deckB'
+  }
+]
 
-
-export const StageView = () => (
-	<div className="container-fluid">
-		<div className="row">
-			<div className="col-md-4">
-				<DeckContainer deckName={decks[0].name} webaudio={webAudio} />
-				{/* <AudioWave deckName={decks[0].name} /> */}
-			</div>
-			<div className="col-md-4">
-				<h1> i t c h </h1>
-				<Mixer decks={decks} webaudio={webAudio} />
-			</div>
-			<div className="col-md-4">
-				<DeckContainer deckName={decks[1].name} webaudio={webAudio} />
-			</div>
-		</div>
-
-	</div>
-)
+export const StageView = () =>
+  <div className='container-fluid'>
+    <div className='row'>
+      <div className='col-md-4'>
+        <DeckContainer deckName={decks[0].name} webaudio={webAudio} />
+      </div>
+      <div className='col-md-4'>
+        <h1> itch </h1>
+        <Mixer decks={decks} webaudio={webAudio} />
+      </div>
+      <div className='col-md-4'>
+        <DeckContainer deckName={decks[1].name} webaudio={webAudio} />
+      </div>
+    </div>
+  </div>
 
 export default StageView
